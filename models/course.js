89816eaf594b4c37b7d4a9bae1207675be0337d0
Anusha M <anusha@anagha.local>
2018-08-20 17:07:31 +0530

    commit

--- models/course.js
+++ models/course.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   var courses = sequelize.define('courses', {
     name: DataTypes.STRING,
-    description: DataTypes.STRING
+    description: DataTypes.STRING,
+    status: DataTypes.STRING
   }, {});
   courses.associate = function(models) {
     // associations can be defined here
--- router.js
+++ router.js
@@ -1,5 +1,7 @@
 var express = require('express');
-
+var CourseController = require('./controllers/CourseController');
+var ChapterController = require('./controllers/ChapterController');
+var QuestionsController = require('./controllers/QuestionsController');
 module.exports = function (app) {
     app.get('/', (req, res) => {
         res.send('The Quiz Api watch at 1332')
